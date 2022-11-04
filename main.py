909c36143fa5514bf217a5f75293681803b3fa1b
Susanne Koljonen <susanne.koljonen@gmail.com>
2022-11-04 22:25:44 +0200

    moved around functionality of widgets and moved styling to .kv file

--- main.py
+++ main.py
@@ -6,52 +6,23 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
-class SayHello(App):
+class MyGrid(Widget):
+    name = ObjectProperty(None)
+
+    # button functionality
+    def btn(self):
+        print("Name: ", self.name.text)
+        self.name.text = ""
+
+
+class MyApp(App):
     def build(self):
-        self.window = GridLayout()
-        self.window.cols = 1
-        self.window.size_hint = (0.6, 0.7)
-        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
-
-        # add widgets to window
-
-        # image widget
-        self.window.add_widget(Image(source="img/alogo4.png"))
-
-        # label widget
-        self.greeting = Label(
-            text="What is your name?",
-            font_size=18,
-            color='#31C485'
-        )
-        self.window.add_widget(self.greeting)
-
-        # text input widget
-        self.user = TextInput(
-            multiline=False,
-            padding_y=(20, 20),
-            size_hint=(1, 0.5)
-        )
-        self.window.add_widget(self.user)
-
-        # button widget
-        self.button = Button(
-            text="GREET",
-            size_hint=(1, 0.5),
-            bold=True,
-            background_color='#31C485',
-            background_normal=""
-        )
-        self.button.bind(on_press=self.callback)
-        self.window.add_widget(self.button)
-
-        return self.window
-
-    def callback(self, instance):
-        self.greeting.text = "Hello " + self.user.text + "!"
+        return MyGrid()
 
 
 if __name__ == "__main__":
-    SayHello().run()
+    MyApp().run()
